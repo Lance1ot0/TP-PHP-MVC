a087e8ce52487a6dcda40f85852146fbe7b6be27
Louis R <95973810+LouisSleep@users.noreply.github.com>
2022-11-15 17:13:04 +0100

    MAJ dump

--- database/dump.sql
+++ database/dump.sql
@@ -1,18 +1,34 @@
-CREATE TABLE IF NOT EXISTS User
+CREATE TABLE IF NOT EXISTS user
 (
     id        INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username  VARCHAR(255) NOT NULL,
     password  VARCHAR(255) NOT NULL,
     email     VARCHAR(255) NOT NULL,
-    firstName VARCHAR(255),
-    lastName  VARCHAR(255),
-    gender    CHAR(1),
-    roles     JSON         NOT NULL
+    first_name VARCHAR(255),
+    last_name  VARCHAR(255),
+    is_admin  BOOLEAN NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Post
+CREATE TABLE IF NOT EXISTS post
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    content VARCHAR(255) NOT NULL,
+    author  VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL,
+    image BLOB,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS comment
 (
     id      INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     content TEXT,
-    author  int NOT NULL
+    author  VARCHAR(255),
+    created_at DATETIME NOT NULL,
+    user_id INT,
+    post_id INT,
+    support_id INT,
+    FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE 
 );
