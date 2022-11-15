45f47e752fe545ebcaf3a5d86693b469c14b0270
Louis R <95973810+LouisSleep@users.noreply.github.com>
2022-11-15 16:25:02 +0100

    initialisation .gitignore

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f883199980e077c2b1c3b1af4f19c4ea5ba15964',
+        'name' => 'rocca/app',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rocca/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f883199980e077c2b1c3b1af4f19c4ea5ba15964',
+            'dev_requirement' => false,
+        ),
+    ),
+);
